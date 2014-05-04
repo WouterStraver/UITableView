01e50a3bb5facd4db98940f5719b266bd9fa6c7f
Wouter Straver <wouterstraver@mbp-van-wouter.lan>
2014-05-04 17:49:37 +0200

    added stuff

---
+++ UITableViewController/WSTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  WSTableViewController.h
+//  UITableViewController
+//
+//  Created by Wouter Straver on 04-05-14.
+//  Copyright (c) 2014 Wouter Straver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSString *section0;
+
+@end
